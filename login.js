1f151a24416b43dd33423b6f8d443894e5134fa2
Samuel <06088@farnborough.ac.uk>
2021-01-25 15:45:51 +0000

    --- player 2 can only input details once player 1's details have been validated --- once player 1's details have been validated, they cannot be changed unless page is refreshed

--- login.js
+++ login.js
@@ -1,34 +1,45 @@
-let username;
-let password;
-let check1 = false;
-let check2 = false;
+let username1;
+let password1;
+let username2;
+let password2;
+let check = false;
 
 function validate1() { // function to validate first account
-    username = document.getElementById("username1").value; // designating inputted username to a variable
-    password = document.getElementById("password1").value; // designating inputted password to a variable
-
-    if (username === "Saam" && password === "skilled") { // checks if inputs are equal to data specified
-        document.getElementById("player1btn").disabled = true; // disables button
-        check1 = true;
-        validate();
+    username1 = document.getElementById("username1").value;
+    password1 = document.getElementById("password1").value;
+    if (username1 === "Saam" && password1 === "skilled") { // checks if inputs are equal to data specified
+        check = true;
+        enableDisable();
     } else {
         alert("wrong username or password");
     }
 }
+
 function validate2() { // function to validate second account --- same as function above but for player 2
-    username = document.getElementById("username2").value
-    password = document.getElementById("password2").value
-
-    if (username === "William" && password === "thick") {
-        document.getElementById("player2btn").disabled = true;
-        check2 = true;
-        validate();
+    username2 = document.getElementById("username2").value;
+    password2 = document.getElementById("password2").value;
+    if (username2 === "William" && password2 === "thick") {
+        window.location.href = "http://localhost:63342/Project-2021-web/game.html?"; // changes html page to that of the game
     } else {
         alert("wrong username or password");
     }
 }
-function validate() {
-    if (check1 && check2) {
-        window.location.href = "http://localhost:63342/Project-2021-web/game.html?";
-    }
+
+function enableDisable() {
+    username1 = document.getElementById("username1");
+    password1 = document.getElementById("password1");
+    username2 = document.getElementById("username2");
+    password2 = document.getElementById("password2");
+
+    username1.disabled = true;
+    password1.disabled = true; // disables player 1 elements as to make sure the user cant change details after submission
+    document.getElementById("player1btn").disabled = true;
+
+    username2.disabled = false;
+    password2.disabled = false; // enables player 2 elements
+    document.getElementById("player2btn").disabled = false; // disables button
 }
+
+document.getElementById("username2").disabled = true;
+document.getElementById("password2").disabled = true; // disables player 2 elements so players have to input player 1 details first
+document.getElementById("player2btn").disabled = true;
